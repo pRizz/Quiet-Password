33d79fe58dff3198bb1010c1e04cc980be783eb7
Peter Ryszkiewicz <pryszkie@gmail.com>
2014-01-16 22:24:37 -0600

    Start of project. Made a test site.

---
+++ test-site/js/main.js
@@ -0,0 +1,57 @@
+// main.js
+
+$(function(){
+	$body = $("body");
+	console.log("body, ", $body);
+	
+	var $passwordField = $("#passwordField");
+	console.log("passwordField, ", $passwordField);
+	var passwordFieldOffset = $passwordField.offset();
+	console.log("passwordFieldOffset.top, ", passwordFieldOffset.top);
+	console.log("passwordFieldOffset.left, ", passwordFieldOffset.left);
+	var passwordFieldPosition = $passwordField.position();
+	console.log("passwordFieldPosition.top, ", passwordFieldPosition.top);
+	console.log("passwordFieldPosition.left, ", passwordFieldPosition.left);
+	
+	var $passwordFieldClone = $passwordField.clone().css({
+		display: "none",
+		"background-color": "#BBFFBB"
+	});
+		
+	$body.append($passwordFieldClone);
+	
+	setTimeout(function(){		
+		$passwordFieldClone.fadeIn(500);
+		// wierd issue if offset is done before fadeIn call
+		$passwordFieldClone.offset({
+			// top: passwordFieldOffset.top+20,
+			// left: passwordFieldOffset.left
+			top: passwordFieldOffset.top,
+			left: passwordFieldOffset.left
+		});
+	});
+	
+	$passwordField.focus(function(evt){
+		console.log("password field focued");
+		// evt.preventDefault();
+	});
+	
+	$passwordFieldClone.focus(function(evt){
+		console.log("password field clone focued");
+		$passwordField.focus();
+		// evt.preventDefault();
+	});
+	
+	$passwordFieldClone.on('keypress', $passwordField, function(evt){
+		console.log("password field clone keypress");
+		evt.preventDefault();
+	});
+	
+	setInterval(function(){
+		console.log("$passwordField.val(): " + $passwordField.val());
+		console.log("$passwordField.innerHTML: " + $passwordField[0].innerHTML);
+	}, 3000);
+	
+	// add tooltip
+	// add way to remove cover
+});
