d9f925c0bf196ac6773fd00b0f459a0254cbf9d5
Peter Ryszkiewicz <pryszkie@gmail.com>
2014-01-20 22:42:30 -0600

    Copied code from test page to quietPassword.js

--- src/js/quietPassword.js
+++ src/js/quietPassword.js
@@ -1,57 +1,82 @@
 // main.js
 
 $(function(){
-	$body = $("body");
-	console.log("body, ", $body);
+	var debugMode = true;
 	
-	var $passwordField = $("input[type='password']");
-	console.log("passwordField, ", $passwordField);
-	if($passwordField.length !== 0){
-		var passwordFieldOffset = $passwordField.offset();
-		console.log("passwordFieldOffset.top, ", passwordFieldOffset.top);
-		console.log("passwordFieldOffset.left, ", passwordFieldOffset.left);
-		var passwordFieldPosition = $passwordField.position();
-		console.log("passwordFieldPosition.top, ", passwordFieldPosition.top);
-		console.log("passwordFieldPosition.left, ", passwordFieldPosition.left);
+	var passwordFieldEmptyColor = "#FFC090"; // light orange
+	var passwordFieldNonEmptyColor = "#BBFFBB"; // light green
 	
-		var $passwordFieldClone = $passwordField.clone().css({
-			display: "none",
-			"background-color": "#BBFFBB"
+	var placeholderText = "Password";
+	
+	var $passwordFields = $("input[type='password']");
+	for(var i = 0; i < $passwordFields.length; i++){
+		var $passwordField = $($passwordFields[i]);
+		// make placeholder color dark gray
+		$("head").append("<style>::-webkit-input-placeholder{color: #666}</style>");
+		var $passwordFieldClone = $passwordField.clone()
+		.css({
+			position: "absolute", // don't affect other divs
+			"background-color": passwordFieldEmptyColor,
+		})
+		.attr("id", $passwordField.attr("id") + i);
+		$passwordFieldClone.attr("placeholder", placeholderText);
+		
+		// insert clone right after the password field in DOM
+		$passwordField.after($passwordFieldClone);
+		// position the password cover directly over the original field
+		$passwordFieldClone.position({
+			of: $passwordField
+		});
+
+		// add and remove placeholder on the clone when the original is focused in/out
+		$passwordField.on("focus", {
+			"$passwordFieldClone": $passwordFieldClone
+		}, function(evt){
+			evt.data.$passwordFieldClone.attr("placeholder", "");
+		});
+		$passwordField.on("focusout", {
+			"$passwordField": $passwordField,
+			"$passwordFieldClone": $passwordFieldClone
+		}, function(evt){
+			if(evt.data.$passwordField.val().length === 0){
+				evt.data.$passwordFieldClone.attr("placeholder", placeholderText);
+			} else {
+				evt.data.$passwordFieldClone.attr("placeholder", "");
+			}
 		});
 		
-		$body.append($passwordFieldClone);
-	
-		setTimeout(function(){		
-			$passwordFieldClone.fadeIn(1);
-			// wierd issue if offset is done before fadeIn call
-			$passwordFieldClone.offset({
-				// top: passwordFieldOffset.top+20,
-				// left: passwordFieldOffset.left
-				top: passwordFieldOffset.top,
-				left: passwordFieldOffset.left
-			});
+		$passwordFieldClone.on("focus", {
+			"$passwordField": $passwordField
+		}, function(evt){
+			evt.data.$passwordField.focus(); // delegate the focus
+		});
+		
+		$passwordField.on("keyup", {
+			"$passwordField": $passwordField,
+			"$passwordFieldClone": $passwordFieldClone
+		}, function(evt){
+			if(debugMode){
+				console.log("passwordField changed");
+				console.log("$passwordField.val(): " + evt.data.$passwordField.val());
+				console.log("$passwordField.val().length: " + evt.data.$passwordField.val().length);
+			}
+			if(evt.data.$passwordField.val().length === 0){
+				evt.data.$passwordFieldClone.css({
+					"background-color": passwordFieldEmptyColor
+				});
+			} else {
+				evt.data.$passwordFieldClone.css({
+					"background-color": passwordFieldNonEmptyColor // light green
+				});
+			}
 		});
 	
-		$passwordField.focus(function(evt){
-			console.log("password field focued");
-			// evt.preventDefault();
-		});
-	
-		$passwordFieldClone.focus(function(evt){
-			console.log("password field clone focued");
-			$passwordField.focus();
-			// evt.preventDefault();
-		});
-	
-		$passwordFieldClone.on('keypress', $passwordField, function(evt){
-			console.log("password field clone keypress");
-			evt.preventDefault();
-		});
-	
-		setInterval(function(){
-			console.log("$passwordField.val(): " + $passwordField.val());
-			console.log("$passwordField.innerHTML: " + $passwordField[0].innerHTML);
-		}, 3000);		
+		if(debugMode){
+			setInterval(function($passwordField){
+				console.log("$passwordField.val(): " + $passwordField.val());
+			}, 3000, $passwordField);	
+		}
+			
 	}
 	
 	// add tooltip
